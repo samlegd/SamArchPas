c506f97f7edfafc5d73797f86ed09c550523c4c4
samlegd <134265633+samlegd@users.noreply.github.com>
2024-09-20 17:59:01 +0200

    aller je tente 245 files je suis un fou

---
+++ Source/thomas/thomas.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class thomas : ModuleRules
+{
+	public thomas(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/thomas/thomasGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "thomasGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AthomasGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AthomasGameMode();
+};
+
+
+
